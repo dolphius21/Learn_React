b3f748d0ce5554b661a561b0a9db11211a3d4d7b
dolphius21 <johnrudolphlee@gmail.com>
2021-07-23 11:58:17 +0800

    Done with effect state & events section

---
+++ src/lessons/44_Effect_State_&_Events/01_Broken_log.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count > 0) {
+      console.log(count);
+    }
+  }, [count]);
+
+  function handleButtonClick() {
+    setCount((prevCount) => prevCount + 5);
+  }
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={handleButtonClick}>Add 5</button>
+    </>
+  );
+}
+
+render(<App />, document.querySelector('#react-root'));
---
+++ src/lessons/44_Effect_State_&_Events/02_Currency_Selector.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+
+function CurrencySelector() {
+  const [currency, setCurrency] = useState('');
+
+  useEffect(() => {
+    if (currency !== '') {
+      console.log(currency);
+    }
+  }, [currency]);
+
+  function handleCurrencyChange(event) {
+    setCurrency(event.target.value);
+  }
+
+  return (
+    <>
+      <h3>Select currency</h3>
+      <select onChange={handleCurrencyChange}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="cad">CAD</option>
+      </select>
+      <h1>{currency}</h1>
+    </>
+  );
+}
+
+render(<CurrencySelector />, document.querySelector('#react-root'));
