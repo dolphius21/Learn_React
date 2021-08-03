ee453e204bc75396b37205cc2081eada1e4159d3
dolphius21 <johnrudolphlee@gmail.com>
2021-08-03 09:23:17 +0800

    Done with JSX advanced II section

---
+++ src/lessons/71_JSX_Advanced_II/01_Translations/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { EN } from './translations.js';
+
+function App() {
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: EN.welcome }}></div>
+      <button dangerouslySetInnerHTML={{ __html: EN.login }}></button>
+    </>
+  );
+}
+
+render(<App />, document.querySelector('#react-root'));
---
+++ src/lessons/71_JSX_Advanced_II/01_Translations/translations.js
@@ -0,0 +1,4 @@
+export const EN = {
+  welcome: 'Welcome to our <strong>Supermarket</strong>',
+  login: 'Login to start <em>shopping</em>'
+};
