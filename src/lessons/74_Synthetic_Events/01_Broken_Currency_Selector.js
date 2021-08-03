2bab78e3099108d96d17882a8726a2b10691bc18
dolphius21 <johnrudolphlee@gmail.com>
2021-08-03 11:39:05 +0800

    Done with synthetic events

---
+++ src/lessons/74_Synthetic_Events/01_Broken_Currency_Selector.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+
+function CurrencySelector() {
+  const [currency, setCurrency] = useState('');
+  const [rate, setRate] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleCurrencyChange(event) {
+    setIsLoading(true);
+    event.persist();
+    fetch(
+      `https://react-tutorial-demo.firebaseio.com/currencies/${event.target.value}.json`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrency(event.target.value);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <h2>Currency selector</h2>
+      <select onChange={handleCurrencyChange} disabled={isLoading}>
+        <option value="">Select a currency</option>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="cad">CAD</option>
+      </select>
+      <h3>
+        Chosen currency: <span id="chosen">{currency.toUpperCase()}</span>
+      </h3>
+    </>
+  );
+}
+
+render(<CurrencySelector />, document.querySelector('#react-root'));
