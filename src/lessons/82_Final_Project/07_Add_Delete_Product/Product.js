9ba34549dd9e09a9b64cd2aee00854a402b78147
dolphius21 <johnrudolphlee@gmail.com>
2021-08-09 09:30:43 +0800

    Done with add delete product

---
+++ src/lessons/82_Final_Project/07_Add_Delete_Product/Product.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button.js';
+
+export default function Product(props) {
+  const { details } = props;
+
+  return (
+    <div className="product">
+      <div className="product-image-container">
+        <Link to={`/products/${details.id}`}>
+          <img
+            src={details.image}
+            width="100"
+            height="100"
+            className="product-image"
+            alt={details.name}
+          />
+        </Link>
+        <div className="product-quantity-container">
+          <div className="product-quantity">0</div>
+        </div>
+      </div>
+      <div className="product-info">
+        <h3>{details.name}</h3>
+        <p>{details.description}</p>
+      </div>
+      <div className="product-checkout">
+        <div>
+          <Button
+            outline
+            className="product-delete"
+            onClick={() => props.onProductDelete(details.id)}
+          >
+            x
+          </Button>
+        </div>
+        <Button outline onClick={() => props.onProductAdd(details)}>
+          ${details.price}
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/lessons/82_Final_Project/07_Add_Delete_Product/index.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar.js';
+import Home from './Home.js';
+import About from './About.js';
+import Products from './Products.js';
+import ProductDetails from './ProductDetails.js';
+import Cart from './Cart.js';
+
+function App() {
+  const [cart, setCart] = useState([]);
+  /* !! DO NOT REMOVE !!*/
+  window.__testsCart = cart; // used for the tests in this project
+
+  useEffect(() => {
+    // to visualize the cart in the console every time in changes
+    // you can also use React dev tools
+    console.log(cart);
+  }, [cart]);
+
+  function handleProductDelete(id) {
+    const updatedCart = cart.filter((product) => product.id !== id);
+    setCart(updatedCart);
+  }
+
+  function handleProductAdd(newProduct) {
+    const existingProduct = cart.find(
+      (product) => product.id === newProduct.id
+    );
+
+    if (existingProduct) {
+      const updatedCart = cart.map((product) => {
+        if (product.id === newProduct.id) {
+          return { ...product, quantity: product.quantity + 1 };
+        }
+        return product;
+      });
+      setCart(updatedCart);
+    } else {
+      const updatedCart = [...cart, { ...newProduct, quantity: 1 }];
+      setCart(updatedCart);
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      <Navbar cart={cart} />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/products">
+            <Products
+              cart={cart}
+              onProductAdd={handleProductAdd}
+              onProductDelete={handleProductDelete}
+            />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails onProductAdd={handleProductAdd} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+render(<App />, document.querySelector('#react-root'));
